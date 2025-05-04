2d39084b976ba1c37688e5e2e56fb3a71d11bdfb
Karman Kekana <thabang@emerg.co.za>
2025-05-04 18:35:33 +0200

    Update StartCall.tsx

--- components/StartCall.tsx
+++ components/StartCall.tsx
@@ -44,7 +44,7 @@ export default function StartCall() {
                     stroke={"currentColor"}
                   />
                 </span>
-                <span>Start Call</span>
+                <span>Talk to Wiinta</span>
               </Button>
             </motion.div>
           </AnimatePresence>
