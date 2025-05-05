63410290aaa1c9883c7342098c6c9a1673c4ab24
tiisetso000 <154628973+tiisetso000@users.noreply.github.com>
2025-05-05 18:22:14 +0100

    Update Wiinta.tsx
    Updated the width

--- components/logos/Wiinta.tsx
+++ components/logos/Wiinta.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import logo from "@/public/wiinta-logo.png"; // Put your logo file in the public folder
 
 const WiintaLogo = (props) => (
-  <Image src={logo} alt="Wiinta Logo" height={32} width={auto} {...props} />
+  <Image src={logo} alt="Wiinta Logo" height={32} width={100} {...props} />
 );
 
 export default WiintaLogo;
